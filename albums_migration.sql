56c216c397a78035263094299dd3ad030891ec96
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-31 16:44:53 -0500

    Finished the update exercise and fixed the albums_migration year colum

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS albums;
    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
    artist VARCHAR(50),
    name VARCHAR(50) NOT NULL,
-   release_date YEAR,
+   release_date Int,
    sales DOUBLE,
    genre VARCHAR(50)
  );
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,20 +1,20 @@
 USE codeup_test_db;
 
 SELECT 'All albums in your table' As '';
-SELECT name FROM albums;
+SELECT sales FROM albums;
 
 UPDATE albums
 SET sales = sales * 10;
 
 SELECT 'All albums in your table updated' As '';
-SELECT name FROM albums;
+SELECT sales FROM albums;
 
 SELECT 'All albums released before 1980' AS '';
 SELECT name FROM albums WHERE release_date < 1980;
 
 UPDATE albums
 SET release_date = 1800
-WHERE release_date = release_date < 1980;
+WHERE release_date < 1980;
 
 SELECT 'All albums released on 1800' AS '';
 SELECT name FROM albums WHERE release_date = 1800;
