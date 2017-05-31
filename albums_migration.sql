751173a15b590be8b6bac0d5dbc8bd73d6970825
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-31 11:34:24 -0500

    Finished creaing the albums table exercise

---
+++ albums_migration.sql
@@ -0,0 +1,12 @@
+ USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+ CREATE TABLE albums (
+   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   artist VARCHAR(50),
+   name VARCHAR(50) NOT NULL,
+   release_date YEAR,
+   sales DOUBLE,
+   genre VARCHAR(50)
+ );
