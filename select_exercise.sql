8d1612ef54a03f62a294840d621ff1c3a7705b3d
Jaime Moncada <jaimemoncada13@gmail.com>
2017-05-31 15:59:35 -0500

    Updated the qureys and albums

--- select_exercise.sql
+++ select_exercise.sql
@@ -1,19 +1,19 @@
 USE codeup_test_db;
 
-SELECT 'The name of all albums by Pink Floyd.' AS 'Exercise 1';
+SELECT 'The name of all albums by Pink Floyd.' AS '';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released.' AS 'Exercise 2';
-SELECT name FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT 'The year Sgt. Pepper''s Lonely Hearts Club Band was released.' AS '';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-SELECT 'The genre for Nevermind.' AS 'Exercise 3';
-SELECT name FROM albums WHERE name = 'Nevermind';
+SELECT 'The genre for Nevermind.' AS '';
+SELECT genre FROM albums WHERE name = 'Nevermind';
 
-SELECT 'Which albums were released in the 1990''s.' AS 'Exercise 4';
+SELECT 'Which albums were released in the 1990''s.' AS '';
 SELECT name FROM albums WHERE release_date between 1990 and 1999;
 
-SELECT 'Which albums had less than 20 million certified sales.' AS 'Exercise 5';
+SELECT 'Which albums had less than 20 million certified sales.' AS '';
 SELECT name FROM albums WHERE sales < 20;
 
-SELECT 'All the albums in the rock genre.' AS 'Exercise 6';
-SELECT * FROM albums WHERE genre = 'Rock';
+SELECT 'All the albums in the rock genre.' AS '';
+SELECT name FROM albums WHERE genre LIKE '%Rock%';
