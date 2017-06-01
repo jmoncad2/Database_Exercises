b652c617786a450b369a804c286fcb13c0a775c1
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 10:21:49 -0500

    Corrected some delete statments that should of been select statments

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
 
-DELETE 'Albums released after 1991' AS '';
+SELECT 'Albums released after 1991' AS '';
 DELETE name FROM albums WHERE release_date > 1991;
-DELETE 'Albums with the genre ''disco''' AS '';
+SELECT 'Albums with the genre ''disco''' AS '';
 DELETE name, genre FROM albums WHERE genre LIKE '%disco%';
-DELETE 'Albums by ''Whitney Houston''(...or maybe an artist of your choice)' AS '';
+SELECT 'Albums by ''Whitney Houston''(...or maybe an artist of your choice)' AS '';
 DELETE name FROM albums WHERE artist = 'Pink Floyd';
