208b5076d94a7eaf285f60a839e9a854da6c6ee7
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 15:21:09 -0500

    Finished where exercise

---
+++ where_exercises.sql
@@ -0,0 +1,21 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q';
