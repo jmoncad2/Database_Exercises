3c7566f70e908aa723e4c977b66a102279df6ab9
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 15:48:07 -0500

    Finished exercise 3 and 4

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,17 +1,33 @@
 USE employees;
 
-SELECT first_name, last_name, gender
+SELECT
+  first_name,
+  last_name,
+  gender
 FROM employees
 WHERE gender = 'M'
-AND (first_name = 'Irena'
-OR first_name = 'Vidya'
-OR first_name = 'Maya');
+      AND (first_name = 'Irena'
+           OR first_name = 'Vidya'
+           OR first_name = 'Maya');
 
-SELECT first_name, last_name
+SELECT
+  first_name,
+  last_name
 FROM employees
-WHERE last_name LIKE 'E%';
+WHERE last_name LIKE 'E%'
+      OR last_name LIKE '%E';
 
-SELECT first_name, last_name, hire_date
+SELECT
+  first_name,
+  last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E';
+
+SELECT
+  first_name,
+  last_name,
+  hire_date
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
