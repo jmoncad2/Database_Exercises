2d70cd973493a94fb54a980ccec6b8ea58d17a87
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 15:58:40 -0500

    Finished all exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,24 +11,19 @@ WHERE gender = 'M'
            OR first_name = 'Maya');
 
 SELECT
-  first_name,
-  last_name
+  *
 FROM employees
 WHERE last_name LIKE 'E%'
       OR last_name LIKE '%E';
 
 SELECT
-  first_name,
-  last_name
+  *
 FROM employees
 WHERE last_name LIKE 'E%'
       AND last_name LIKE '%E';
 
 SELECT
-  first_name,
-  last_name,
-  hire_date,
-  birth_date
+  *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%12-25';
@@ -39,4 +34,5 @@ WHERE birth_date LIKE '%12-25';
 
 SELECT *
 FROM employees
-WHERE last_name LIKE '%q';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
