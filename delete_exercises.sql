3c17aa8210755dba9556831cf2142eed6cc6126c
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 10:23:30 -0500

    corrected the 3 delete lines to match proper syntax

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,8 +1,8 @@
 USE codeup_test_db;
 
 SELECT 'Albums released after 1991' AS '';
-DELETE name FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
 SELECT 'Albums with the genre ''disco''' AS '';
-DELETE name, genre FROM albums WHERE genre LIKE '%disco%';
+DELETE FROM albums WHERE genre LIKE '%disco%';
 SELECT 'Albums by ''Whitney Houston''(...or maybe an artist of your choice)' AS '';
-DELETE name FROM albums WHERE artist = 'Pink Floyd';
+DELETE FROM albums WHERE artist = 'Pink Floyd';
