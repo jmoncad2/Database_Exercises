4243f526fa532dd98318aedd3912eba15e5d240e
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 15:50:49 -0500

    Finished 5 exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -27,9 +27,11 @@ WHERE last_name LIKE 'E%'
 SELECT
   first_name,
   last_name,
-  hire_date
+  hire_date,
+  birth_date
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%12-25';
 
 SELECT *
 FROM employees
