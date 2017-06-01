c29ce3ffb345c3f46164c3c7f591935aca1fa985
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 16:54:03 -0500

    Refactored some code

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -36,8 +36,9 @@ ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-      AND birth_date LIKE '%12-25';
+WHERE hire_date LIKE '199%'
+      AND birth_date LIKE '%12-25'
+ORDER BY birth_date, hire_date DESC;
 
 SELECT *
 FROM employees
