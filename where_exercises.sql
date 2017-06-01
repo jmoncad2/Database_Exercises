eb024d4d23a145420bc1708eac99b2e1d0eeabe1
Jaime Moncada <jaimemoncada13@gmail.com>
2017-06-01 15:45:53 -0500

    Finished 1 and 2 second exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,8 +1,11 @@
 USE employees;
 
-SELECT first_name, last_name
+SELECT first_name, last_name, gender
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE gender = 'M'
+AND (first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya');
 
 SELECT first_name, last_name
 FROM employees
